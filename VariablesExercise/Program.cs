bff48d6d5a529c9a72891c45111b436ea0965be7
Tom Settle <tom.k.settle@gmail.com>
2023-02-03 12:13:04 -0600

    Added some variable usage to Main().

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string myName = "Tom";
+            int myAge = 54;
+            char myGroup = 'B';
+            bool isAlive = true;
+            double interestRate = 5.6375;
+            decimal rentalRate = 47.5m;
+
+            Console.WriteLine($"My name is {myName}, and my age is {myAge} years old.");
+            Console.WriteLine($"I am in Group {myGroup}.");
+            if ( isAlive ) 
+                {
+                Console.WriteLine("I am, in fact, alive.");
+                }
+            else 
+                {
+                Console.WriteLine("I regret to say that I am no longer alive.");
+                }
+            Console.WriteLine($"An interest rate might be stored as a Double, such as {interestRate}. Whereas an hourly rental rate could be a Decimal, such as {rentalRate}.");
         }
     }
 }
